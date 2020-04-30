4309b76cacba607e3793a607951cdccc54bdc350
khaoula <tti.khaoula@gmail.com>
2020-04-30 14:52:18 +0100

    chechpoint React ajouté

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container"> 
+                  
+        <div class="row" id="content"> 
+     
+          <div id="formulaire" class="col-md-6"> 
+                         
+             <h2>Formulaire avec Bootstrap</h2>
+                         
+            <form>
+                              <div class="form-group row">
+                                  <label for="inputPassword3" class="col-sm-2 col-form-label">Name</label>
+                                  <div class="col-sm-8">
+                                    <input type="text" class="form-control" id="inputPassword3" placeholder="Name"></input>
+                                  </div>
+                                </div>
+                              <div class="form-group row">
+                                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                                <div class="col-sm-8">
+                                  <input type="email" class="form-control" id="inputEmail3" placeholder="Email"></input>
+                                </div>
+                              </div>
+                              <div class="form-group row">
+                                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                                <div class="col-sm-8">
+                                  <input type="password" class="form-control" id="inputPassword3" placeholder="Password"></input>
+                                </div>
+                              </div>
+                             
+                              
+                              <div class="form-group row">
+                                <div class="col-sm-10">
+                                  <button type="submit" class="btn btn-primary">Sign in</button>
+                                </div>
+                              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
